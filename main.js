a90b6c992ca3fb03e5c57827efc0ba8bf1345c8c
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-03-05 02:12:31 +0900

    js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const modal = document.getElementById('easyModal');
 const modalOpen = document.getElementById('modalOpen');
 
 // 閉じるボタン取得
-const modalClose = document.getElementsByClassName('modalClose');
+const modalClose = document.getElementsByClassName('modalClose')[0];
 
 // クリックで開くイベントリスナー
 modalOpen.addEventListener('click',openClick);
